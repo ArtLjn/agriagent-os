81820241211a46644e6d4456d1f021ba2f374bb0
BlockShip <1521505611@qq.com>
2026-06-24 14:07:40 +0800

    chore: remove tracked generated artifacts

--- archive/FarmManagerMobile/__mocks__/react-native-vector-icons.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Text } from 'react-native';
-
-const Icon = ({ name, size, color, style }) => (
-  <Text style={[{ fontSize: size, color }, style]}>{name}</Text>
-);
-
-export default Icon;
